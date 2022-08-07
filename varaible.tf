3ccdf606ab8cdb6bd7a802f9b7dcbf63e21e8c96
Maheshcloud <59549736+Maheshcloud@users.noreply.github.com>
2022-08-07 22:38:49 +0530

    Update varaible.tf

--- varaible.tf
+++ varaible.tf
@@ -6,3 +6,12 @@ variable "cluster-name" {
 variable "AWS_REGION" {
   default = "us-east-1"
 }
+
+variable "ACCESS_KEY" {
+ default = "AKIARHVDGYJYRJ3X3E55"
+  }
+
+variable "SECRET_KEY" {
+  default = "uAUt+UUOxYq0sDr1/1FojPcqBhXbFcOLZZNpLun6"
+}
+
