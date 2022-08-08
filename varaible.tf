b96997127e15248b4d5b3df7120f112f40a28a0f
Maheshcloud <59549736+Maheshcloud@users.noreply.github.com>
2022-08-08 21:27:38 +0530

    Update varaible.tf

--- varaible.tf
+++ varaible.tf
@@ -7,4 +7,12 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 
+variable "ACCESS_KEY" {
+ default = "AKIAZXMEAJ5QI5JR6NEU"
+  }
+
+variable "SECRET_KEY" {
+  default = "Fk49LCN97VozYe+AKboT48T4b7WVM1v4kzWgESpv"
+}
+
 
